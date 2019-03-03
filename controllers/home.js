57988731687d50ca277a81220e997bbd407ba4ce
Quentin <borhink@gmail.com>
2019-03-03 17:54:31 +0100

    vote ok from home page

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,6 @@
 exports.index = function (req, res) {
     const returnResponseOfFileJson = function (content) {
-        res.render('home', {cats: content});
+        res.render('home', {cat1: content[0], cat2: content[1]});
     };
 
     cats.done(returnResponseOfFileJson);
--- server.js
+++ server.js
@@ -8,14 +8,29 @@ cats = database.load('database.json');
 exphbs = require('express-handlebars');
 
 app = express();
+
+const hbs = exphbs.create({
+    helpers: {
+        vote: function (winner, looser) {
+            console.log('winner: ' + winner + ', looser: ' + looser);
+        }
+    },
+    defaultLayout: 'main'
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Config
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public/css'));
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
 
 // Routing
 require('./routing/home');
 require('./routing/vote');
+
+app.get('/', function(req, res) {
+    res.redirect('/home');
+});
 
 app.listen(8080);
