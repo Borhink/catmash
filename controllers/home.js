448ebbdc81d7a58176c2666a3f2c19d1a12b0827
Quentin <borhink@gmail.com>
2019-03-03 15:37:13 +0100

    clickable images

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,6 @@
 exports.index = function (req, res) {
     const returnResponseOfFileJson = function (content) {
-        res.render('home', {cats: content[0].id});
+        res.render('home', {cats: content});
     };
 
     cats.done(returnResponseOfFileJson);
--- server.js
+++ server.js
@@ -4,12 +4,12 @@ Promise = require('bluebird');
 fs = Promise.promisifyAll(require('fs'));
 log = require('./lib/log');
 database = require('./lib/database');
-app = express();
 cats = database.load('database.json');
 exphbs = require('express-handlebars');
 
+app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('public/css'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
