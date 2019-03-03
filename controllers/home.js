2362afc9be3730421a49ac09ec0d4fceaf2e2f9e
Quentin <borhink@gmail.com>
2019-03-03 20:13:49 +0100

    select cat in bdd for vote OK

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,15 @@
 exports.index = function (req, res) {
+    const getCats = function (data) {
+        return data.filter(function(cat){return cat.voted===false})
+            .sort(function(a, b){return b.votes < a.votes})
+        ;
+    };
+
     const returnResponseOfFileJson = function (content) {
         res.render('home', {cat1: content[0], cat2: content[1]});
     };
-
-    cats.done(returnResponseOfFileJson);
+    
+    cats.then(getCats)
+        .then(returnResponseOfFileJson)
+    ;
 }
--- controllers/vote.js
+++ controllers/vote.js
@@ -2,12 +2,9 @@ exports.index = function (req, res) {
     const winner = req.body.winner ? req.body.winner : null;
     const looser = req.body.looser ? req.body.looser : null;
 
-    const returnResponseOfFileJson = function (content) {
-        res.json(content);
-    };
-    const applyVote = function (data) {
-
-        for (cat of data) {
+    const applyVote = async function (data) {
+        
+        for await (cat of data) {
             if (cat.id === winner) {
                 cat.voted = true;
                 cat.votes++;
@@ -15,6 +12,14 @@ exports.index = function (req, res) {
                 cat.voted = true;
             }
         }
+        catsLeft = data.filter(function(cat){return cat.voted===false});
+        if (catsLeft.length < 2)
+            data.forEach(function(cat) {cat.voted = false;});
+        return data;
+    };
+    
+    const returnResponseOfFileJson = function (content) {
+        res.json(content);
     };
 
     const returnError = function () {
